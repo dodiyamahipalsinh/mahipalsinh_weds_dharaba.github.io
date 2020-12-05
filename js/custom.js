0035d80ba09db739b6a3d60205cbea746bb9a140
MahipalSinh <dodiyamahipal93@gmail.com>
2020-12-05 21:24:01 +0530

    final
    final

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
         $(document).ready(function () {
-        var dym_date = 'Dec 10 2020';
-       var given_str = dym_date + " 23:00:00 (Pacific Standard Time)";
+       var dym_date = 'Dec 10 2020';
+       var given_str = dym_date + " 23:30:00 (Pacific Standard Time)";
         
        var countDownDate = new Date(given_str);
        var countDownDate = countDownDate.setHours(countDownDate.getHours());
@@ -39,7 +39,7 @@
             if (distance < 0)
             {
                 clearInterval(x);
-                document.getElementById("count_down_time").innerHTML = "EXPIRED";
+                document.getElementById("count_down_time").innerHTML = "Finally We Meet";
             }
         }, 1000);
         
@@ -50,3 +50,42 @@
             $( "#count_down_time" ).hide();
             $( ".timer-main" ).hide();
         });
+
+
+  // window.onload = function() {
+  //   document.addEventListener("contextmenu", function(e){
+  //     e.preventDefault();
+  //   }, false);
+  //   document.addEventListener("keydown", function(e) {
+  //   //document.onkeydown = function(e) {
+  //     // "I" key
+  //     if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
+  //       disabledEvent(e);
+  //     }
+  //     // "J" key
+  //     if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
+  //       disabledEvent(e);
+  //     }
+  //     // "S" key + macOS
+  //     if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+  //       disabledEvent(e);
+  //     }
+  //     // "U" key
+  //     if (e.ctrlKey && e.keyCode == 85) {
+  //       disabledEvent(e);
+  //     }
+  //     // "F12" key
+  //     if (event.keyCode == 123) {
+  //       disabledEvent(e);
+  //     }
+  //   }, false);
+  //   function disabledEvent(e){
+  //     if (e.stopPropagation){
+  //       e.stopPropagation();
+  //     } else if (window.event){
+  //       window.event.cancelBubble = true;
+  //     }
+  //     e.preventDefault();
+  //     return false;
+  //   }
+  // };
